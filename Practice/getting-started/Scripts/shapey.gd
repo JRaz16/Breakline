042d67c6680965436856d2413747f99e73340f9c
JRazMarist <John.Rasizzi1@marist.edu>
2026-01-29 13:49:44 -0500

    1/29/26 In class activity
    Added sprites and made physics and collision work

---
+++ Practice/getting-started/Scripts/shapey.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+
+const SPEED = 400.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	#if not is_on_floor():
+	#	velocity += get_gravity() * delta
+
+	# Handle jump.
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	#	velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction :Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	if direction:
+		velocity = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
+	
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+		if collider is RigidBody2D:
+			var push_direction = -collision.get_normal()
+			collider.apply_central_impulse(push_direction * SPEED)
+			
