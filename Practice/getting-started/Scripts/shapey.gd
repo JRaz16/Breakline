e44c0f326fbd8736d9d1d41cedee9d4b01b45cd6
JRazMarist <John.Rasizzi1@marist.edu>
2026-02-05 13:44:18 -0500

    Animations

--- Practice/getting-started/Scripts/shapey.gd
+++ Practice/getting-started/Scripts/shapey.gd
@@ -6,22 +6,29 @@ const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
+	if motion_mode == MOTION_MODE_GROUNDED:
+		
 	# Add the gravity.
-	#if not is_on_floor():
-	#	velocity += get_gravity() * delta
+		if not is_on_floor():
+			velocity += get_gravity() * delta
 
 	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	#	velocity.y = JUMP_VELOCITY
-
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		var direction:float = Input.get_axis("move_left", "move_right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+	else:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction :Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	if direction:
-		velocity = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		var direction :Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+		if direction:
+			velocity = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
 	
@@ -31,4 +38,4 @@ func _physics_process(delta: float) -> void:
 		if collider is RigidBody2D:
 			var push_direction = -collision.get_normal()
 			collider.apply_central_impulse(push_direction * SPEED)
-			
+				
